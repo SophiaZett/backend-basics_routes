81dfaedc6ab435cf682d663e77a30d16c7ebbe1a
Sophia S. <sophia0791@icloud.com>
2024-01-23 12:31:57 +0100

    change the word port

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ import { server } from "./server.js";
 
 const port = 8080;
 server.listen(port, () => {
-  console.log(`Server running at http://127.0.0.1:${prt}/`);
+  console.log(`Server running at http://127.0.0.1:${port}/`);
 });
